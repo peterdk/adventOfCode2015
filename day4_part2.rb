1c7c1395080884f4f6b232ece8406fa7e1050f2a
Peter <peterdekraker@umito.nl>
2015-12-08 23:58:34 +0100

    first 5 days minus 5part2

---
+++ day4_part2.rb
@@ -0,0 +1,17 @@
+require 'digest'
+
+input = "yzbqklnj"
+index = -1;
+output = nil
+while (output == nil || output.chars.take(6).count{|c|c=="0"} != 6)
+index += 1
+md5 = Digest::MD5.new
+md5 << input
+md5 << index.to_s
+output = md5.hexdigest
+if (index % 1000 == 0)
+puts index
+end
+end
+puts output
+puts index
